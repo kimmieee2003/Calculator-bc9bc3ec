f812fb4cf6328bb6c0099644b1a9dc6e0c2e5181
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-16 16:20:43 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,18 @@
+<?php
+
+echo ("Welke operatie wil je uitvoeren? (+, -)");
+$uitvoering = readline();
+if($uitvoering == "+"){
+echo ("Eerste getal?");
+$Eerste = readline();
+echo ("Tweede getal?");
+$Tweede = readline();
+echo (int) ($Eerste + $Tweede);
+} elseif($uitvoering == "-") {
+    echo ("Eerste getal?");
+    $Derde = readline();
+    echo ("Tweede getal?");
+    $Vierde = readline();
+    echo (int) ($Derde - $Vierde);
+}
+?>
